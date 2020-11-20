60022c084a5fa3d6c6e3ce07cd596b512cffa049
Damian Parniewicz <damianp@man.poznan.pl>
2020-11-20 08:58:30 +0100

    utils modifications

--- utils/udp_send.py
+++ utils/udp_send.py
@@ -6,14 +6,29 @@ import time
 client = InfluxDBClient(host='localhost', use_udp=True, udp_port=8089)
 
 
-TIMESTAMP_0 =  1000*1000*int(time.time()) # in us
+TIMESTAMP_0 =  1000*1000*1000*int(time.time()) # in ns
 CNT = 10
 print("\nLoading database with %d entries\n" % CNT)
 
                                     
 line_body = []
 for i in range(CNT):
-    line_body.append("int,srcip=10.0.0.1,dstip=10.0.0.2,sctp=1111,dstp=80 seq=%d,srcts=%d,dstts=%d,delay=%d,sink_jitter=%d,reordering=0 %d" % 
-                                    (i, TIMESTAMP_0 + i, TIMESTAMP_0 + i + 1, i ,i, TIMESTAMP_0 + i+1))
+    line_body.append("int_telemetry,srcip=10.0.0.1,dstip=10.0.0.2,sctp=1111,dstp=80 seq=%d,srcts=%di,dstts=%di,delay=%di,sink_jitter=%di,reordering=0i %di" % 
+                                    (i, TIMESTAMP_0 + i, TIMESTAMP_0 + i + 1, i , i, TIMESTAMP_0 + i+1))
 
-client.send_packet(line_body, time_precision='u',protocol='line')
+
+print(line_body[0])
+message = '\n'.join(line_body) + '\n'
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.sendto(message, ('localhost', 8089))
+
+#  data = ('\n'.join(packet) + '\n').encode('utf-8')
+
+#client.send_packet(line_body, time_precision='u',protocol='line')
+
+#~ 1605801680000000i
+#~ 1605801680000001i
+#~ 1605801680000001i
+#~ 1605801227270915992i,
+#~ 1605801228186204176i,
+#~ 1605801228186204176i
